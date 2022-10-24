696d63406b8704e51ac5225f09460804f3837053
Tom <98466837+TomPARIS1@users.noreply.github.com>
2022-10-24 17:26:39 +0200

    Modif BDD + ajout select atelier reservation

--- atelier.sql
+++ atelier.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 03 Octobre 2022 à 07:19
+-- Généré le :  Lun 24 Octobre 2022 à 15:23
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -19,6 +19,20 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `atelier`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `atelier`
+--
+
+CREATE TABLE `atelier` (
+  `id_atelier` int(11) NOT NULL,
+  `plan` text NOT NULL,
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `id_compte` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -41,10 +55,23 @@ CREATE TABLE `compte` (
 
 CREATE TABLE `etagere` (
   `id` int(11) NOT NULL,
-  `xMin` double NOT NULL,
-  `xMax` double NOT NULL,
-  `yMin` double NOT NULL,
-  `yMax` double NOT NULL
+  `x` double NOT NULL,
+  `y` double NOT NULL,
+  `id_atelier` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `historique`
+--
+
+CREATE TABLE `historique` (
+  `id_historique` int(11) NOT NULL,
+  `id_outil` int(11) NOT NULL,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -62,9 +89,29 @@ CREATE TABLE `outil` (
   `y` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id_reservation` int(11) NOT NULL,
+  `id_outil` int(11) NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `atelier`
+--
+ALTER TABLE `atelier`
+  ADD PRIMARY KEY (`id_atelier`),
+  ADD KEY `id_compte` (`id_compte`);
 
 --
 -- Index pour la table `compte`
@@ -76,7 +123,15 @@ ALTER TABLE `compte`
 -- Index pour la table `etagere`
 --
 ALTER TABLE `etagere`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_atelier` (`id_atelier`);
+
+--
+-- Index pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD PRIMARY KEY (`id_historique`),
+  ADD KEY `id_outil` (`id_outil`);
 
 --
 -- Index pour la table `outil`
@@ -86,9 +141,21 @@ ALTER TABLE `outil`
   ADD KEY `id_etagere` (`id_etagere`);
 
 --
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_reservation`),
+  ADD KEY `id_outil` (`id_outil`);
+
+--
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `atelier`
+--
+ALTER TABLE `atelier`
+  MODIFY `id_atelier` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `compte`
 --
@@ -100,19 +167,53 @@ ALTER TABLE `compte`
 ALTER TABLE `etagere`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `historique`
+--
+ALTER TABLE `historique`
+  MODIFY `id_historique` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `outil`
 --
 ALTER TABLE `outil`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id_reservation` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- Contraintes pour les tables exportées
 --
+
+--
+-- Contraintes pour la table `atelier`
+--
+ALTER TABLE `atelier`
+  ADD CONSTRAINT `atelier_ibfk_1` FOREIGN KEY (`id_compte`) REFERENCES `compte` (`id`);
+
+--
+-- Contraintes pour la table `etagere`
+--
+ALTER TABLE `etagere`
+  ADD CONSTRAINT `etagere_ibfk_1` FOREIGN KEY (`id_atelier`) REFERENCES `atelier` (`id_atelier`);
+
+--
+-- Contraintes pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD CONSTRAINT `historique_ibfk_1` FOREIGN KEY (`id_outil`) REFERENCES `outil` (`id`);
 
 --
 -- Contraintes pour la table `outil`
 --
 ALTER TABLE `outil`
   ADD CONSTRAINT `outil_ibfk_1` FOREIGN KEY (`id_etagere`) REFERENCES `etagere` (`id`);
+
+--
+-- Contraintes pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`id_outil`) REFERENCES `outil` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
