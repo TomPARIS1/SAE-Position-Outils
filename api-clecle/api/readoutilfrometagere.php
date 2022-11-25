3c6a427fdc50d1fabb0b2b5d2b2bbd8e2ceb8f80
Alex <98466389+JustAlex14@users.noreply.github.com>
2022-11-25 11:28:32 +0100

    API Etagere
    WIP API Atelier + Reservation

---
+++ api-clecle/api/readoutilfrometagere.php
@@ -0,0 +1,47 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    include_once '../config/database.php';
+    include_once '../class/outil.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $items = new Outil($db);
+
+    $etagere_id = isset($_GET['id_etagere']) ? $_GET['id_etagere'] : die();
+
+    $stmt = $items->getOutilsFromEtagere($etagere_id);
+    $itemCount = $stmt->rowCount();
+    
+    if($itemCount > 0){
+        
+        $employeeArr = array();
+        $employeeArr["body"] = array();
+        $employeeArr["itemCount"] = $itemCount;
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "id" => $id,
+                "id_etagere" => $id_etagere,
+                "type" => $type,
+                "nbr_utilisations" => $nbr_utilisations,
+                "x" => $x,
+                "y" => $y
+            );
+
+            array_push($employeeArr["body"], $e);
+        }
+        echo json_encode($employeeArr);
+    }
+
+    else{
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "No record found.")
+        );
+    }
+    
+?>
---
+++ api-clecle/api/updateoutil.php
@@ -0,0 +1,67 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    
+    include_once '../config/database.php';
+    include_once '../class/outil.php';
+    
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $item = new Outil($db);
+    $item->id = isset($_GET['id']) ? $_GET['id'] : die();
+    $item->getOutilsFromId();
+    
+    if($item->type != null){
+        
+        if (isset($_GET['type']))
+        {
+            $item->type = $_GET['type'];
+        }
+    
+        if (isset($_GET['id_etagere']))
+        {
+            $item->id_etagere = $_GET['id_etagere'];
+        }
+    
+        if (isset($_GET['nbr_utilisations']))
+        {
+            $item->nbr_utilisations = $_GET['nbr_utilisations'];
+        }
+    
+        if (isset($_GET['x']))
+        {
+            
+            $item->x = $_GET['x'];
+        }
+
+        if (isset($_GET['y']))
+        {
+            
+            $item->y = $_GET['y'];
+        }
+      
+        $result = $item->updateOutil(); 
+
+        $emp_arr = array(
+            "id" =>  $item->id,
+            "id_etagere" => $item->id_etagere,
+            "nbr_utilisations" => $item->nbr_utilisations,
+            "type" => $item->type,
+            "x" => $item->x,
+            "y" => $item->y,
+            "result" => $result
+        );
+      
+        http_response_code(200);
+        echo json_encode($emp_arr);
+    }
+      
+    else{
+        http_response_code(404);
+        echo json_encode("Recipe not found.");
+    }
+?>
