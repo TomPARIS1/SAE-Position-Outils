35195ef7cd86e27b02b786eedc10b9ab41618a4a
Tom <98466837+TomPARIS1@users.noreply.github.com>
2022-12-12 15:31:44 +0100

    Add scripts + page Inscription

---
+++ SAEClecle/api-clecle/api/readetagerfromatelier.php
@@ -0,0 +1,45 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    
+    include_once '../config/database.php';
+    include_once '../class/etagere.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $items = new Etagere($db);
+
+    $atelier_id = isset($_GET['id_atelier']) ? $_GET['id_atelier'] : die();
+
+    $stmt = $items->getEtagereFromAtelier($atelier_id);
+    $itemCount = $stmt->rowCount();
+    
+    if($itemCount > 0){
+        
+        $employeeArr = array();
+        $employeeArr["body"] = array();
+        $employeeArr["itemCount"] = $itemCount;
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $e = array(
+                "id" => $id,
+                "id_atelier" => $id_atelier,
+                "x" => $x,
+                "y" => $y
+            );
+
+            array_push($employeeArr["body"], $e);
+        }
+        echo json_encode($employeeArr);
+    }
+
+    else{
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "No record found.")
+        );
+    }
+    
+?>
