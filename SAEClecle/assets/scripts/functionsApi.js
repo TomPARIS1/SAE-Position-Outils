424852178d52ce399b26966a65b2557468e3e277
Tom <98466837+TomPARIS1@users.noreply.github.com>
2023-01-03 13:46:40 +0100

    Ajout fonction "connexion"

--- SAEClecle/assets/scripts/functionsApi.js
+++ SAEClecle/assets/scripts/functionsApi.js
@@ -1,4 +1,5 @@
 const apiUrl = 'localhost/my-app/api-clecle/api/';
+console.log("hello")
 
 function getAtelierFromAccount (compte_id) {
     let atelier = fetch(apiUrl + 'readatelier.php/' + '?id_compte=' + compte_id);
@@ -33,17 +34,37 @@ function getOutil (id_etagere) {
 }
 
 function addUser (email, mdp) {
-    fetch(apiUrl + 'compte/createcompte.php', {
-        method: "POST",
+    fetch('../api-clecle/api/compte/createcompte.php', {
+        method: 'POST',
         headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json; charset=UTF-8'
         },
         body: JSON.stringify({nom: email, mdp: mdp, niveau: 1})
-    }).then(res => res.json())
-      .then(res => console.log(res))
+    }).then((res) => console.log(res.statusText));
 }
 
-function setReservation () {
+function connexion (email, mdp) {
+    const element = document.getElementById('errorConnexion');
+
+    fetch('../api-clecle/api/compte/checkcompte.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({nom: email, mdp: mdp})
+    }).then((res) => res.json())
+    .then(res => {
+        console.log(res);
+        if (res['codeErr'] === '0') {
+            window.location = "index.html";
+        }
+        else if (res['codeErr'] === '1') {
+            element.innerHTML = "<h4>Erreur : le mot de passe est incorrect.</h4>";
+        }
+        else {
+            element.innerHTML = "<h4>Erreur : l'adresse mail est incorrecte.</h4>"; 
+        }
+    });
 
 }
+
